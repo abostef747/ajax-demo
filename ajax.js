c6c934af93e2ad4e758bdd3236abcc5b9281e387
abostef747 <s.abostef@yahoo.com>
2016-05-02 11:53:22 +0200

    Update ajax.js

--- ajax.js
+++ ajax.js
@@ -1,6 +1,22 @@
+// JavaScript
+// Vi ska använda AJAX för att hämta innehållet på en hemsida
+
+// Vad är AJAX? 
+// Det är inte ett fotbollslag...
+// Det är inte ett diskmedel...
+// Det är inte ett programmeringsspråk...
+
+// ...det är en teknik, en kombination av saker, för att hämta data från URLs
+// AJAX - Asynchronous JavaScript and XML
+
+// Påbörja en hämtning
 var request = new XMLHttpRequest();
-request.onreadystatechange = function(){
-console.log("något Hände");
+
+request.onreadystatechange = function() {
+	console.log("Nånting hände!");
+    // I variabeln res (response) finns det information...
+    console.log("Svaret (response) är " + request.responseText);
 }
-request.open('GET', 'http://www.google.com');
+
+request.open('GET', 'http://mardby.se/AJK15G/lorem_text.php');
 request.send();
